26f16b03bb7b9313fdf5a725479b119c12b74aea
Yingjie <1275963@gmail.com>
2016-09-27 22:07:56 -0400

    Cleaning code for submission

--- ass1.m
+++ ass1.m
@@ -8,15 +8,21 @@ for i = 1:init_point
     [xy(1,i), xy(2,i)] = ginput(1);
 end
 
+
 xy = xy(:, 1:init_point);
-t = 0:0.01:1;
+t = 0:0.01:1; %resolution set as 0.01
 
 hold on
 axis([0,1,0,1])
-plot(xy(1,:),xy(2,:), 'c*-');
+plot(xy(1,:),xy(2,:), 'c*-'); 
 
 N = size(xy, 2);
+% For b(i,j,k) 
+% i: stands for subindexing, 1-4
+% j:stands for global indexing
+% k: stands for xy 1-2
 b = zeros(4, N-3, 2);
+
 % First row
 b(1, 1, :) = xy(:, 1);
 b(2, 1, :) = xy(:, 2);
@@ -28,24 +34,26 @@ b(2, N-3, :) = (xy(:, N-2) + xy(:, N-1))./2;
 b(3, N-3, :) = xy(:, N-1);
 b(4, N-3, :) = xy(:, N);
 
-
+% compute the middle points first, because they only relay on d
 for jj = 2: N-4
     b(2, jj, :) = 2*xy(:, jj + 1)/3 + xy(:, jj + 2)/3;
     b(3, jj, :) = xy(:, jj + 1)/3 + 2*xy(:, jj + 2)/3;
 end
 
+% based on the middle points, compute the first and last points
 for jj = 2: N - 4
    b(4, jj, :) = (b(3, jj, :) + b(2, jj + 1, :))./2;
    b(1, jj + 1, :) = b(4, jj, :);
 end
 
+% do the plot
 for i = 1: N-3
     b_mat = squeeze(b(:,i,:));
     b_mat_points = b_mat';
     
     last_bpoint_plot = plot(b_mat_points(1,:),b_mat_points(2,:), 'x:');
     
-    c_xy = bezier3(t, b_mat');
+    c_xy = bezier3(t, b_mat'); % using stanard formula
     last_bazier_plot = plot(c_xy(1,:), c_xy(2,:));
 end
 
--- lerp.m
+++
@@ -1,7 +0,0 @@
-%
-% to perform affine interpolation between two points p, q
-% w.r.t. an affine basis [r, s]
-%
-function pt = lerp(p, q, r, s, t)
-pt = ((s - t)/(s - r))* p + ((t - r)/(s - r))* q;
-end
